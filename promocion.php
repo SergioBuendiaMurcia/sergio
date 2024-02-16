526b2128d338e7a4ac4d6590d779acec0253e4a1
SergioBuendiaMurcia <97170065+SergioBuendiaMurcia@users.noreply.github.com>
2024-02-16 21:30:48 +0100

    Add files via upload

---
+++ promocion.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ *	Script que se usa en los endpoints para trabajar con registros de la tabla promocion
+ *	La clase "promocion.class.php" es la clase del modelo
+*/
+require_once 'src/response.php';
+require_once 'src/classes/promocion.class.php';
+require_once 'src/classes/auth.class.php';
+
+$auth = new Authentication();
+$auth->verify();
+
+$promocion = new Promocion();
+
+/**
+ * Se mira el tipo de petición que ha llegado a la API y dependiendo de ello se realiza una u otra accción
+ */
+switch ($_SERVER['REQUEST_METHOD']) {
+	/**
+	 * Si se ha recibido un GET se llama al método get() del modelo y se le pasan los parámetros recibidos por GET en la petición
+	 */
+	case 'GET':
+		$params = $_GET;
+
+		$promociones = $promocion->get($params);
+
+		$response = array(
+			'result' => 'ok',
+			'promociones' => $promociones
+		);
+
+		Response::result(200, $response);
+
+		break;
+		
+	/**
+	 * Si se recibe un POST, se comprueba si se han recibido parámetros y en caso afirmativo se usa el método insert() del modelo
+	 */
+	case 'POST':
+		$params = json_decode(file_get_contents('php://input'), true);
+
+		if(!isset($params)){
+			$response = array(
+				'result' => 'error',
+				'details' => 'Error en la solicitud'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+
+		$insert_id = $promocion->insert($params);
+
+		$response = array(
+			'result' => 'ok',
+			'insert_id' => $insert_id
+		);
+
+		Response::result(201, $response);
+		break;
+
+	/**
+	 * Cuando es PUT, comprobamos si la petición lleva el id del jugador que hay que actualizar. En caso afirmativo se usa el método update() del modelo.
+	 */
+	case 'PUT':
+		$params = json_decode(file_get_contents('php://input'), true);
+
+		if(!isset($params) || !isset($_GET['id']) || empty($_GET['id'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'Error en la solicitud'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		$promocion->updatePut($_GET['id'], $params);
+
+		$promocion->updatePatch($_GET['id'], $params);
+
+		$response = array(
+			'result' => 'ok'
+		);
+
+		Response::result(200, $response);	
+		break;
+
+	/**
+	 * Cuando se solicita un DELETE se comprueba que se envíe un id de jugador. En caso afirmativo se utiliza el método delete() del modelo.
+	 */
+	case 'DELETE':
+		if(!isset($_GET['id']) || empty($_GET['id'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'Error en la solicitud'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		$promocion->delete($_GET['id']);
+
+		$response = array(
+			'result' => 'ok'
+		);
+
+		Response::result(200, $response);
+		break;
+
+	/**
+	 * Para cualquier otro tipo de petición se devuelve un mensaje de error 404.
+	 */
+	default:
+		$response = array(
+			'result' => 'error'
+		);
+
+		Response::result(404, $response);
+
+		break;
+}
+?>
